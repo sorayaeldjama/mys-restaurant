81bfc097f9f2fd005a8e0f32de90557f6226eb84
soraya <sorayaeldjama@gmail.com>
2024-10-12 20:31:36 +0200

    creating navBar _second commit

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
-import { Box,Typography,Divider,AppBar } from "@mui/material";
 import { NavLink, } from "react-router-dom";
+import {
+    AppBar,
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    Toolbar,
+    Typography,
+  } from "@mui/material";
+  import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar =()=>{
 const [mobileNavBarOpen,setMobileNavBarOpen]=useState(false);
@@ -40,11 +49,73 @@ const navBarMobile =(
     </Box>
 )
     return(
+        <>
   <Box>
-    <AppBar>
-
-    </AppBar>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
+        <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{
+                mr: 2,
+                display: { sm: "none" },
+              }}
+              onClick={onClickMobileNavBar}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              {/* <img src={Logo} alt="logo" height={"70"} width="250" /> */}
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <NavLink activeClassName="active" to={"/"}>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/menu"}>Menu</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/about"}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/contact"}>Contact</NavLink>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileNavBarOpen}
+            onClose={onClickMobileNavBar}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: "240px",
+              },
+            }}
+          >
+            {navBarMobile}
+          </Drawer>
+        </Box>
+        <Box>
+          <Toolbar />
+        </Box>
   </Box>
+  <div>fhdfdjh</div>
+
+</>
     )
 }; 
 export default Navbar ; 
