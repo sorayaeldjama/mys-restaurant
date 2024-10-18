c1131097baf9146d38f9c1f8aeeb7c2b54d89a66
soraya <sorayaeldjama@gmail.com>
2024-10-18 19:44:28 +0200

    Lyout flex footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/Home.js';
 import Footer from './components/Layout/Footer.js'
 import Menu from './pages/Menu.js'
 import NotFound from './pages/NotFound.js';
+import About from './pages/About.js'
+import Contact from './pages/Contact.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/Menu" element={<Menu/>}></Route>
+      <Route path="/About" element={<About/>}></Route>
+      <Route path="/Contact" element={<Contact/>}></Route>
 
       <Route path="/Footer" element={<Footer/>}></Route>
       <Route path="*" element={<NotFound />} />
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,29 @@
 import React from "react";
-import Navbar from "./Navbar.js"
-import Footer from "./Footer"
-const Layout =({children})=>{
-return(
-    <>
-<Navbar></Navbar>
-<div>{children}</div>
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Box } from "@mui/material";
 
-<Footer></Footer>
+const Layout = ({ children }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh", // Assure que la page occupe toute la hauteur de la fenêtre
+      }}
+    >
+      {/* Navbar */}
+      <Navbar />
 
-    </>
-);
+      {/* Contenu principal (flex: 1 prend tout l'espace disponible) */}
+      <Box sx={{ flex: 1 }}>
+        {children}
+      </Box>
+
+      {/* Footer toujours en bas */}
+      <Footer />
+    </Box>
+  );
 };
-export default Layout ; 
+
+export default Layout;
